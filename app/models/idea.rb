5a879934a5dbc49aef75519b7908d29b15ec4c09
Rika Rumiyati <rika.rumiyati@gmail.com>
2021-02-20 22:50:17 +0700

    define model relation and reference

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ApplicationRecord
   mount_uploader :picture, PictureUploader
   enum status: { active: 1, inactive: 0}
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_152935) do
+ActiveRecord::Schema.define(version: 2021_02_20_154626) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_02_20_152935) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_02_20_152935) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "ideas", "users"
 end
