16c336eeced2c304e8f8ac8103fc03608bf0fd8b
gemmaan <gemma.nur@btpn.com>
2021-03-16 14:14:41 +0700

    Add new page home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :users
   get 'pages/info'
-  root to: redirect('/ideas')
+  # root to: redirect('/ideas')
   resources :ideas
   resources :statuses, only: :update
+  root 'home#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
